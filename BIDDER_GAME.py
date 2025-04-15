c7e8b65be44b8ef27b72808fe2c775a2547024e7
=Anurag <=anuragsnd@gmail.com>
2025-04-15 15:38:25 +0530

    game

--- BIDDER_GAME.py
+++ BIDDER_GAME.py
@@ -1,1 +1,28 @@
-print("Hello world")
+bidders = {}
+print("Welcome to online bidding ")
+
+
+def take_bid():
+    name = input("Please enter your name \n")
+    bid = input("what is your bidding amount is $\n")
+    bidders[name] = bid
+
+
+def find_highest_bid():
+    highest_bid = 0
+    winner = ""
+    for name in bidders:
+        bidding_value = int(bidders[name])
+        if bidding_value >= highest_bid:
+            highest_bid = bidding_value
+            winner = name
+
+    print(f"The winner is {winner} with the highest bid of {highest_bid}$")
+
+
+while True:
+    take_bid()
+    option = input("does anyone else want to make the bid??\n").lower()
+    if option != "y":
+        find_highest_bid()
+        break
